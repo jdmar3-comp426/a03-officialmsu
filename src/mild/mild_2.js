ecccf80a89b0be8898527a6d48108591fa6334ed
officialmsu <mhsudeveloper@gmail.com>
2021-11-15 03:59:16 -0500

    Update mild_2.js

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -82,7 +82,7 @@ export function identifyArray(array) {
  */
 export function removeKey(object, key) {
    delete object[key];
-   
+
    return object;
 }
 
@@ -103,6 +103,11 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
+   newobject = object;
+
+   delete object[key];
+
+   return newobject + " " + object;
 
 }
 
