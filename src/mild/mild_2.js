dd25d9bd2ec48bb0ae2e7c27f86543465e617030
officialmsu <mhsudeveloper@gmail.com>
2021-11-15 03:56:42 -0500

    Update mild_2.js

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -81,9 +81,8 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-   if (object[key] == 'password'){
-      return key;
-   }
+   delete object[key];
+   
    return object;
 }
 
